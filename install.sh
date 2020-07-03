0663b9e3176f90a936bb3424db521e180346b421
Eduardo Gomez <edugomez2299@gmail.com>
2020-07-03 12:03:31 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 cd ~
-mv vimrc .vim && cd .vim
-mkdir undor-dir sessions
-vim -c VimscriptPlugins -c qa
+if [ ! -d ".vim" ]; then
+	# echo "no existe"
+	mv vimrc .vim && cd .vim
+	mkdir undor-dir sessions
+	vim -c VimscriptPlugins -c qa
+else
+	echo "Do yo want to delete .vim?[y/N]"
+	read del
+	if [[ -z $del || $del == "y" ]]; then
+		echo "me vale"
+		rm -rf .vim
+	fi
+fi
 
